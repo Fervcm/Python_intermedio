5779e2eb562ea301cda84f27706992b3788c0c35
Fernando Vacca <Ferchovamo@hotmail.com>
2022-06-10 01:52:24 -0500

    Primer sesion de python intermedio

---
+++ lists_and_dits.py
@@ -0,0 +1,29 @@
+from multiprocessing import Value
+
+
+def run():
+    my_list = [1, 'Hello', True, 4.5]
+    my_dict = {'firstname': 'Fernando', 'lastname': 'Vacca'}
+    
+    super_list = [
+        {'firstname': 'Fernando', 'lastname': 'Vacca'},
+        {'firstname': 'Natalia', 'lastname': 'Vacca'},
+        {'firstname': 'Liliana', 'lastname': 'Gomez'},
+        {'firstname': 'Sneider', 'lastname': 'Roble'}
+    ]
+
+    super_dict= {
+        'natural_nums': [1, 2, 3, 4, 5],
+        'integer_nums': [-1, -2, 0, 1, 2],
+        'floating_nums': [1.1, 4.5, 6.43]
+    }
+     
+    #for key, value in super_list.items():
+     #   print(key, '-', value)
+        
+    for value in super_list:
+        print(value)
+        
+        
+if __name__ == '__main__':
+    run()
