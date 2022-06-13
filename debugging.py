98d5d78380a6d79bf3d22f7616bb18a48ab4e407
Fernando Vacca <Ferchovamo@hotmail.com>
2022-06-12 20:51:55 -0500

    Se crea debugging.py para verificar errores con visual code.

---
+++ debugging.py
@@ -0,0 +1,18 @@
+from time import process_time_ns
+
+
+def divisors(num):
+    divisors = []
+    for i in range(1, num +1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+def run():
+    num = int(input("Ingresa un numero: "))
+    
+    print(divisors(num))
+    print("El programa termino")
+
+if __name__ == "__main__":
+    run()
