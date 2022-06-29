b0a5cf04a451d5c538844e475491a2b1ba2acc63
Fernando Vacca <Ferchovamo@hotmail.com>
2022-06-28 22:08:37 -0500

    Se inicia proyecto ahorcado

---
+++ ahorcado/ahorcado.py
@@ -0,0 +1,13 @@
+from distutils.command.clean import clean
+import os
+
+
+def run():
+    
+    os.system("clear")
+    print("¡Adivina la palabra!")
+    input("Ingrese una letra: ")
+    
+
+if __name__=="__main__":
+    run()
